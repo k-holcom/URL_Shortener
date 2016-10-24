3871c4bcd854ec71e70519f19fff49537d3ec9e1
Kimerlee Holcom <kaholcom@fullsail.edu>
2016-10-24 15:42:28 -0400

    basic static API created

---
+++ src/routes/routes.js
@@ -0,0 +1,29 @@
+//Setting up main routes file as a module for other Routes
+module.exports = (express) => {
+  /***
+  ** Creating a constant that will allow all routes for the API to go through
+  ** the Router() method
+  ***/
+  const API = express.Router();
+
+  //URL Route
+  API.post('/url', (req, res) => {
+    var short = '';
+    var str = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+    //Sets up the end of the shortened URL
+    for(var i = 0; i < 6; i++){
+      var rand = Math.floor(Math.random() * (62 - 0) + 0);
+      var char = str.substring(rand, rand+1);
+      short = short + char;
+    }
+    var shortened = 'phnx.wd/' + short;
+    res.json({
+      url: 'www.risingphoenixwd.com',
+      string: shortened
+    });
+  })
+
+  //Returns the API
+  return API
+}
--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 // Setting up Server for the API
 
-//Set up variables and constants for the API Server
+// Set up variables and constants for the API Server
 const express = require('express');
 const body_parser = require('body-parser');
 
+//Activate Express
 const app = express();
+
+//Activate Body-Parser
+
+// Port set to 3000 as specified by assignment instructions
 const port = 3000;
 
-//Setting up the actual Server
+//Configure Routes
+app.use('/api/v1', require('./routes/routes.js')(express));
 
+// Setting up the actual Server
 app.listen(port, function(){
   console.log('Server Active on Port', port);
 });
