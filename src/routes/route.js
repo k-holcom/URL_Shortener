a1b654d7ed4d08b9cdea251055bac1130ada7ba3
Kimerlee Holcom <kaholcom@fullsail.edu>
2016-11-02 16:08:12 -0400

    routes/route.js is up to airbnb standards
    There is a warning, not an error that is associated w/ a console.log in
    the suppress() method

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,33 @@
-//Setting up main routes file as a module for other Routes
+// Setting up main routes file as a module for other Routes
 module.exports = (express) => {
-  //Creating a constant that will allow all routes for the API to go through the Router() method
+  // Creating a constant that will allow all routes for the API to go through the Router() method
   const api = express.Router();
-
-  //URL Route
+  // Function to suppress console.log messages and print to a file
+  const suppress = (msg) => {
+    const debugVar = process.env.DEBUG;
+    const date = new Date().toISOString();
+    const message = date + ' => ' + msg + '\r\n';
+    if (debugVar === 'true') {
+      console.log(message);
+    } else if (debugVar === 'false') {
+      const fs = require('fs');
+      fs.appendFile('./logs/debugLog.log', message, (err) => {
+        if (err) throw err;
+      });
+    }
+  };
+  // URL Route
   api.get('/status', (req, res) => {
-    console.log('The /status route has been hit');
+    suppress('The /status route has been hit');
     res.json({
-      healthy: true
-    })
+      healthy: true,
+    });
   });
 
   api.use('/api/v1/', require('./api/url')(express));
 
   api.use('/', require('./api/goTo')(express));
 
-  //Returns the API
+  // Returns the API
   return api;
-}
+};
