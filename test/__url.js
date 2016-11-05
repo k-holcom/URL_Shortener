ce876c30f4ab25e139e1c7c6541630347f382f7b
Kimerlee Holcom <kaholcom@fullsail.edu>
2016-11-04 21:31:50 -0400

    Just some clean up

--- test/__url.js
+++ test/__url.js
@@ -90,15 +90,15 @@ if(process.env.DEBUG === 'true'){
       // This string will hold the information that will be used to create the random information after phnx.wd
       var str = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-      //Sets up the end of the shortened URL
+      // Sets up the end of the shortened URL
       for(var i = 0; i < 6; i++){
-        //chooses a random number between 1 and 62. This is to account for 0-9, A-Z, and a-z.
+        // chooses a random number between 1 and 62. This is to account for 0-9, A-Z, and a-z.
         var rand = Math.floor(Math.random() * (str.length - 0) + 0);
 
-        //Find the character that is at the index of the randomly selected number.
+        // Find the character that is at the index of the randomly selected number.
         var char = str.substring(rand, rand + 1);
 
-        //Adds the character to the variable short
+        // Adds the character to the variable short
         short = short + char;
       }
       const shortURL = short;
